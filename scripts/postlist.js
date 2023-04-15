16cab7e80e52733601ee9ea61048181fa98bebf5
Johan John <coderhacker603@gmail.com>
2023-04-15 19:32:21 +0530

    My first project commit after part B

---
+++ scripts/postlist.js
@@ -0,0 +1,12 @@
+var state=true
+function background(id){
+    if(state){
+        document.getElementById(id).style.boxShadow='5px 5px 10px 0 purple'
+        state=false;
+    }
+    else{
+        document.getElementById(id).style.boxShadow=''
+        state=true;
+    }
+
+}
